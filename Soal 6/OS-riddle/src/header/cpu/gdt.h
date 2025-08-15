a3c871015cf8f211faef0e2390826f0790222f24
ITBAtharizza <18223079@std.stei.itb.ac.id>
2025-08-15 19:34:35 +0700

    [soal 6] feat: SegmentDescriptor in gdt.h

--- Soal 6/OS-riddle/src/header/cpu/gdt.h
+++ Soal 6/OS-riddle/src/header/cpu/gdt.h
@@ -13,7 +13,29 @@
 extern struct GDTR _gdt_gdtr;
 
 struct SegmentDescriptor {
-    // implementasi bagian ini
+    // First 32-bit (Bytes 0-3)
+    uint16_t segment_low;  // This is the Limit (bits 0-15)
+    uint16_t base_low;     // Base (bits 0-15)
+
+    // Next 16-bit (Bytes 4-5)
+    uint8_t base_mid;      // Base (bits 16-23)
+
+    // This is the Access Byte (Byte 5)
+    uint8_t type_bit   : 4; // Type fields (A, RW, DC, E)
+    uint8_t non_system : 1; // S bit (1 for code/data)
+    uint8_t dpl        : 2; // Descriptor Privilege Level
+    uint8_t present    : 1; // Present bit
+
+    // Next 16-bit (Bytes 6-7)
+    // This byte contains high limit and flags
+    uint8_t limit_high : 4; // This is segment_low's upper bits
+    uint8_t avl        : 1; // Available bit
+    uint8_t l_bit      : 1; // Long mode bit
+    uint8_t db_bit     : 1; // D/B bit (size)
+    uint8_t g_bit      : 1; // Granularity bit
+
+    uint8_t base_high;     // Base (bits 24-31)
+
 } __attribute__((packed));
 
 struct GlobalDescriptorTable {
