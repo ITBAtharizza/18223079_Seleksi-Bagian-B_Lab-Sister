a418a82a94cc88d732672b2019b3b32a3d04d01e
ITBAtharizza <18223079@std.stei.itb.ac.id>
2025-08-15 15:56:56 +0700

    [soal 6] feat: fill interrupt.c features

--- Tahap 1/Soal 6/OS-riddle/src/code/cpu/interrupt.c
+++ Tahap 1/Soal 6/OS-riddle/src/code/cpu/interrupt.c
@@ -44,8 +44,53 @@ void pic_remap(void) {
     out(PIC2_DATA, PIC_DISABLE_ALL_MASK);
 }
 
+// Syscall handler for clearing the framebuffer.
+void syscall_clear_screen(void) {
+    framebuffer_clear();
+    framebuffer_state.cur_col = 0;
+    framebuffer_state.cur_row = 0;
+}
+
+// Syscall handler for reading the RTC.
+void syscall_get_time(uint32_t ebx, uint32_t ecx, uint32_t edx) {
+    uint8_t hour, minute, second;
+    read_rtc(&hour, &minute, &second);
+    
+    // Return time values to user-space via pointers
+    *((uint8_t*)ebx) = hour;
+    *((uint8_t*)ecx) = minute;
+    *((uint8_t*)edx) = second;
+}
+
+
 void syscall(struct InterruptFrame frame) {
-    // implementasi bagian ini
+    // Extract syscall number and arguments from registers for clarity
+    uint32_t syscall_num = frame.cpu.general.eax;
+    uint32_t arg1        = frame.cpu.general.ebx;
+    uint32_t arg2        = frame.cpu.general.ecx;
+    uint32_t arg3        = frame.cpu.general.edx;
+
+    switch (syscall_num) {
+        case 0:  *((int8_t*)arg2) = read(*(struct FAT32DriverRequest*)arg1); break;
+        case 1:  *((int8_t*)arg2) = read_directory(*(struct FAT32DriverRequest*)arg1); break;
+        case 2:  *((int8_t*)arg2) = write(*(struct FAT32DriverRequest*)arg1); break;
+        case 3:  *((int8_t*)arg2) = delete(*(struct FAT32DriverRequest*)arg1); break;
+        case 4:  get_keyboard_buffer((char*)arg1, (int32_t*)arg2); break;
+        case 5:  putchar((char)arg1, arg2); break;
+        case 6:  puts((char*)arg1, arg2, arg3); break;
+        case 7:  keyboard_state_activate(); break;
+        case 8:  *((uint32_t*)arg2) = move_to_child_directory(*(struct FAT32DriverRequest*)arg1); break;
+        case 9:  *((uint32_t*)arg2) = move_to_parent_directory(*(struct FAT32DriverRequest*)arg1); break;
+        case 10: list_dir_content((char*)arg1, arg2); break;
+        case 11: print((char*)arg1, arg2); break;
+        case 12: print_path_to_dir((char*)arg1, arg2, (char*)arg3); break;
+        case 13: syscall_clear_screen(); break;
+        case 14: process_destroy(arg1); break;
+        case 15: process_create_user_process(*(struct FAT32DriverRequest*)arg1); break;
+        case 16: ps((char*)arg1); break;
+        case 17: syscall_get_time(arg1, arg2, arg3); break;
+        case 18: *((int8_t*)arg2) = move_dir(*(struct FAT32DriverRequest*)arg1, *(struct FAT32DriverRequest*)arg2); break;
+    }
 }
 
 void main_interrupt_handler(struct InterruptFrame frame) {
